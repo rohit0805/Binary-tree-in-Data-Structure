aad05a1c989cee57736d7fbff2f3cf7340a9a22b
Rohit <rohit5822540@gmail.com>
2020-04-23 10:39:31 +0530

    BFS of a binary tree(levelorder)

--- tree.cpp
+++ tree.cpp
@@ -27,95 +27,6 @@ void levelorder(struct node *root){
 	}
 	cout<<endl;
 }
-void insertion(struct node *root,int data){
-	if(root==NULL){
-		return;
-	}
-	queue<struct node*> q;
-	q.push(root);
-	while(!q.empty()){
-		root=q.front();
-		q.pop();
-		if(root->left==NULL){
-			root->left=create(data);
-			return;
-		}
-		else{
-			q.push(root->left);
-		}
-		if(root->right==NULL){
-			root->right=create(data);
-			return;
-		}
-		else{
-			q.push(root->right);
-		}
-	}
-}
-void deletedeep(struct node *root,struct node* dnode){
-	queue<struct node*> q;
-	q.push(root);
-	struct node *temp;
-	while(!q.empty()){
-		temp=q.front();
-		q.pop();
-		if(temp==dnode){
-			temp=NULL;
-			delete(dnode);
-			return;
-		}
-		if(temp->right){
-			if(temp->right==dnode){
-				temp->right=NULL;
-				delete(dnode);
-				return;
-			}
-			else{
-				q.push(temp->right);
-			}
-		}
-		if(temp->left){
-			if(temp->left==dnode){
-				temp->left=NULL;
-				delete(dnode);
-				return;
-			}
-			else{
-				q.push(temp->left);
-			}
-		}
-	}
-}
-struct node* deletion(struct node *root,int data){
-	if(root==NULL)
-		return NULL;
-	if(root->left==NULL && root->right==NULL){
-		if(root->data==data)
-			return NULL;
-		else
-			return root;
-	}
-	queue<struct node*> q;
-	q.push(root);
-	struct node *remp,*temp,*keynode=NULL;
-	while(!q.empty()){
-		temp=q.front();
-		q.pop();
-		if(temp->data==data){
-			keynode=temp;
-		}
-		if(temp->left)
-			q.push(temp->left);
-		if(temp->right)
-			q.push(temp->right);
-	}
-	if(keynode!=NULL){
-		int x=temp->data;
-		deletedeep(root,temp);
-		keynode->data=x;
-	}
-	return root;
-}
 int main(){
 	struct node *root=create(1);
 	root->left=create(2);
@@ -124,10 +35,6 @@ int main(){
 	root->left->right=create(5);
 	root->right->left=create(6);
 	root->right->right=create(7);
-	cout<<"Before deletion"<<endl;
-	levelorder(root);
-	cout<<"After deletion"<<endl;
-	root=deletion(root,3);
 	levelorder(root);
 	return 0;
 }
