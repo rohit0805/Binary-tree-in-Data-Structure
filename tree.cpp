639ce29b4caa396edaef2af394696fe802c6a8f5
Rohit <rohit5822540@gmail.com>
2020-04-27 10:35:20 +0530

    Print all nodes at distance k from a given node

--- tree.cpp
+++ tree.cpp
@@ -12,24 +12,57 @@ struct node* create(int data){
 	newnode->left=newnode->right=NULL;
 	return newnode;
 }
-void printkdistant(struct node *root,int k){
-	if(root==NULL)
+void printnode(struct node *root,int k){
+	if(root==NULL){
 		return;
+	}
 	if(k==0){
 		cout<<root->data<<" ";
 		return;
 	}
-	printkdistant(root->left,k-1);
-	printkdistant(root->right,k-1);
+	printnode(root->left,k-1);
+	printnode(root->right,k-1);
 }
+int printkdistantnode(struct node *root,struct node *target,int k){
+	if(root==NULL){
+		return -1;
+	}
+	else if(root==target){
+		printnode(root,k);
+		return 0;
+	}
+	int dl=printkdistantnode(root->left,target,k);
+	if(dl!=-1){
+		if(dl+1==k){
+			cout<<root->data<<" ";
+		}
+		else{
+			printnode(root->right,k-dl-2);
+		}
+		return dl+1;
+	}
+	int dr=printkdistantnode(root->right,target,k);
+	if(dr!=-1){
+		if(dr+1==k){
+			cout<<root->data<<" ";
+		}
+		else{
+			printnode(root->left,k-dr-2);
+		}
+		return dr+1;
+	}
+	return -1;
+}
+
 int main(){
-	struct node *root=create(1);
-	root->left=create(2);
-	root->right=create(3);
+	struct node *root=create(20);
+	root->left=create(8);
+	root->right=create(22);
 	root->left->left=create(4);
-	root->left->right=create(5);
-	root->right->left=create(6);
-	root->right->right=create(7);
-	printkdistant(root,1);
+	root->left->right=create(12);
+	root->left->right->left=create(10);
+	root->left->right->right=create(14);
+	struct node *target=root->left->right;
+	printkdistantnode(root,target,2);
 	return 0;
 }
