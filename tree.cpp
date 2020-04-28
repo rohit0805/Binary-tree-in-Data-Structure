73824c07ad55cb87385f4d7fa59f674b0d0a4ea9
Rohit <rohit5822540@gmail.com>
2020-04-28 09:08:08 +0530

    children sum property

--- tree.cpp
+++ tree.cpp
@@ -6,106 +6,38 @@ struct node{
 	struct node *left,*right;
 };
 
-struct node* create(int data){
+struct node* newNode(int data){
 	struct node *newnode=new node;
 	newnode->data=data;
 	newnode->left=newnode->right=NULL;
 	return newnode;
 }
-void printnode(struct node *root,int k){
-	if(root==NULL){
-		return;
-	}
-	if(k==0){
-		cout<<root->data<<" ";
-		return;
-	}
-	printnode(root->left,k-1);
-	printnode(root->right,k-1);
-}
-void lefty(struct node *root,int level,int &max_level){
-	if(root==NULL){
-		return ;
-	}
-	if(level>max_level){
-		cout<<root->data<<" ";
-		max_level=level;
-	}
-	lefty(root->left,level+1,max_level);
-	lefty(root->right,level+1,max_level);
-}
-
-void leftview(struct node *root){
-	int max_level=0;
-	lefty(root,1,max_level);
-}
-void righty(struct node *root,int level,int &max_level){
-	if(root==NULL)
-		return;
-	if(level>max_level){
-		cout<<root->data<<" ";
-		max_level=level;
-	}
-	righty(root->right,level+1,max_level);
-	righty(root->left,level+1,max_level);
-}
-void rightview(struct node *root){
-	int max_level=0;
-	righty(root,1,max_level);
-}
-void topview(struct node *root,map<int,pair<int,int>> &hash,int hor,int height){
-	if(root==NULL)
-		return;
-	if(hash.find(hor)==hash.end()){
-		hash[hor]=make_pair(root->data,height);
-	}
-	else{
-		pair<int,int>p=(hash.find(hor))->second;
-		if(p.second>height){
-			hash[hor]=make_pair(root->data,height);
-		}
-	}
-	topview(root->left,hash,hor-1,height+1);
-	topview(root->right,hash,hor+1,height+1);
-}
-void bottomview(struct node *root,map<int,pair<int,int>> &hash,int hor,int height){
-	if(root==NULL)
-		return;
-	if(hash.find(hor)==hash.end()){
-		hash[hor]=make_pair(root->data,height);
-	}
-	else{
-		pair<int,int>p=(hash.find(hor))->second;
-		if(height>p.second){
-			hash[hor]=make_pair(root->data,height);
-		}
-	}
-	bottomview(root->left,hash,hor-1,height+1);
-	bottomview(root->right,hash,hor+1,height+1);
+int issumproperty(struct node *root){
+    if(root==NULL || (root->left==NULL && root->right==NULL)){
+        return 1;
+    }
+    int leftsum=0,rightsum=0;
+    if(root->left)
+        leftsum=root->left->data;
+    if(root->right)
+        rightsum=root->right->data;
+    if(root->data==leftsum+rightsum && issumproperty(root->left) && issumproperty(root->right)){
+        return 1;        
+    }
+    return 0;
 }
 int main(){
-	struct node *root=create(20);
-	root->left=create(8);
-	root->right=create(22);
-	root->left->left=create(4);
-	root->left->right=create(12);
-	root->left->right->left=create(10);
-	//root->left->right->right=create(14);
-	cout<<"Left view of a tree: ";
-	leftview(root);
-	cout<<endl;
-	cout<<"Right view of a tree: ";
-	rightview(root);
-	cout<<"\nTop view of a tree: ";
-	map<int,pair<int,int>> hash,has;
-	topview(root,hash,0,1);
-	for(auto i:hash){
-		cout<<(i.second).first<<" ";
-	}
-	cout<<"\nBottom view of a tree: ";
-	bottomview(root,has,0,1);
-	for(auto i:has){
-		cout<<(i.second).first<<" ";
-	}
+	struct node *root = newNode(10); 
+    root->left     = newNode(8); 
+    root->right = newNode(2); 
+    root->left->left = newNode(3); 
+    root->left->right = newNode(5); 
+    root->right->right = newNode(2); 
+    if(issumproperty(root)) 
+        cout << "The given tree satisfies "
+            << "the children sum property "; 
+    else
+        cout << "The given tree does not satisfy "
+            << "the children sum property "; 
 	return 0;
 }
