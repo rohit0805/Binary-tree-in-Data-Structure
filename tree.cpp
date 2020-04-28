a938404a2403a01d1c25aa0b983ee4c2cb8d2cf9
Rohit <rohit5822540@gmail.com>
2020-04-28 10:06:31 +0530

    Maximum width of a binary tree

--- tree.cpp
+++ tree.cpp
@@ -12,34 +12,31 @@ struct node* newNode(int data){
 	newnode->left=newnode->right=NULL;
 	return newnode;
 }
-int height(struct node *root){
-    if(root==NULL)return 0;
-    return 1+max(height(root->left),height(root->right));
-}
-int isbalanced(struct node *root){
+int maxwidth(struct node *root){
     if(root==NULL)
-        return 1;
-    int leftheight=0,rightheight=0;
-    leftheight=height(root->left);
-    rightheight=height(root->right);
-    if(abs(leftheight-rightheight)<=1 && isbalanced(root->left) && isbalanced(root->right)){
-        return 1;
-    }
-    return 0;
-}
-int isbalancedfast(struct node *root,int *height){
-    int lh=0,rh=0,l,r;
-    if(root==NULL){
-        *height=0;
-        return 1;
-    }
-    l=isbalancedfast(root->left,&lh);
-    r=isbalancedfast(root->right,&rh);
-    *height=(lh>rh?lh:rh)+1;
-    if(abs(lh-rh)>=2)
         return 0;
-    else
-        return l&&r;
+    int count=0,maximum=INT_MIN;
+    queue<struct node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        root=q.front();
+        q.pop();
+        if(root!=NULL){count++;}
+        if(root==NULL){
+            maximum=max(maximum,count);
+            count=0;
+            if(!q.empty())
+                q.push(NULL);
+        }
+        else{
+            if(root->left)
+                q.push(root->left);
+            if(root->right)
+                q.push(root->right);
+            }
+    }
+    return maximum;
 }
 int main(){
 	struct node* root = newNode(1); 
@@ -47,17 +44,9 @@ int main(){
     root->right = newNode(3); 
     root->left->left = newNode(4); 
     root->left->right = newNode(5); 
-    root->left->left->left = newNode(8); 
-    int height=0;
-    if (isbalanced(root)) 
-        cout << "Tree is balanced"; 
-    else
-        cout << "Tree is not balanced";
-    cout<<endl;
-    if (isbalancedfast(root,&height)) 
-        cout << "Tree is balanced"; 
-    else
-        cout << "Tree is not balanced"; 
-    
+    //root->right->left = newNode(6);
+    //root->right->right=newNode(7); 
+    root->left->left->left=newNode(8);
+    cout<<maxwidth(root)<<endl;    
 	return 0;
 }
